43d85edf7c4ef9943c6a57ab1bcb4537cd101db0
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2020-08-11 15:30:35 +0700

    Update Baca_4.java

--- app/src/main/java/com/rendi/covidfight/Baca_4.java
+++ app/src/main/java/com/rendi/covidfight/Baca_4.java
@@ -9,6 +9,6 @@ public class Baca_4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_baca_4);
+        setContentView(R.layout.activity_baca_5);
     }
 }
